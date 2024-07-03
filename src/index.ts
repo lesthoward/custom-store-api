1f104562a46aabd8e5482208e5be6bc50d615ea9
howardlester <lester.howard@source360group.com>
2024-07-02 18:47:13 -0600

    increase request limit size to 250mb

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,26 @@ import morgan from 'morgan';
 import 'dotenv/config';
 import { threekitRoutes } from './routes/api.routes';
 import cors from 'cors';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 
 const app: Application = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(
+    express.json({
+        limit: '250mb',
+    })
+);
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: '250mb',
+        parameterLimit: 50000,
+    })
+);
 app.use(morgan('dev'));
 app.use(express.static('public'));
-app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.json({ limit: '250mb' }));
 
 // CORS
 app.use(cors());
